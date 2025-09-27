7c04730b351be5959231c81305d56f2d2fca17a1
mtanksl <20389632+mtanksl@users.noreply.github.com>
2025-09-27 00:32:07 -0300

    Slider with mouse click and move love.mousereleased

--- LOVELi/Slider.lua
+++ LOVELi/Slider.lua
@@ -115,11 +115,11 @@ function LOVELi.Slider:init(layoutmanager) -- override
 		local thumbsize = self:getdesiredwidth() / (self:getmaximum() - self:getminimum() + 1)
 		local oldvalue = self:getvalue()		
 		local newvalue = math.min(self:getmaximum(), math.max(self:getminimum(), math.floor( (x - (self:getscreenx() + self:getmargin():getleft() ) ) / thumbsize) ) )
-		if oldvalue ~= newvalue and newvalue >= self:getminimum() and newvalue <= self:getmaximum() then
+		if oldvalue ~= newvalue then
 			onvaluechange(oldvalue, newvalue)
 		end
 	end)
-	layoutmanager:subscribe("mousemoved", self, function(x, y, button, istouch, presses)
+	layoutmanager:subscribe("mousemoved", self, function(x, y, dx, dy, istouch)
 		if pressed then
 			local thumbsize = self:getdesiredwidth() / (self:getmaximum() - self:getminimum() + 1)
 			local oldvalue = self:getvalue()
